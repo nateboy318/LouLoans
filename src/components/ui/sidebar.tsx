9b8ee921d95af2ea4d5eb3ec91ee5a3763f93d9d
nateboy318 <mcgamer5hundred@gmail.com>
2024-12-03 13:02:42 -0500

    icon fix sidebar type

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,6 +10,7 @@ import {
   UsersRound,
   Landmark,
 } from "lucide-react";
+import { type LucideIcon } from "lucide-react";
 import { Button } from "./button";
 import { useWindowWidth } from "@react-hook/window-size";
 
@@ -18,7 +19,7 @@ type Props = {};
 export default function Sidebar({}: Props) {
   const onlyWidth = useWindowWidth();
   const mobileWidth = onlyWidth < 1068;
-
+  
   const [isCollapsed, setIsCollapsed] = React.useState(mobileWidth);
   const [isMounted, setIsMounted] = React.useState(false);
 
@@ -34,60 +35,62 @@ export default function Sidebar({}: Props) {
     setIsCollapsed(!isCollapsed);
   }
 
-  interface NavLink {
-    title: string;
-    href: string;
-    icon: LucideIcon;
-    variant: "default" | "ghost";
-  }
+ interface NavLink {
+  title: string;
+  href: string;
+  icon: LucideIcon;
+  variant: "default" | "ghost";
+}
 
-  const links: NavLink[] = [
-    {
-      title: "Dashboard",
-      href: "/",
-      icon: LayoutDashboard,
-      variant: "default",
-    },
-    {
-      title: "Users",
-      href: "/users",
-      icon: UsersRound,
-      variant: "ghost",
-    },
-    {
-      title: "Loans",
-      href: "/loans",
-      icon: Landmark,
-      variant: "ghost",
-    },
-    {
-      title: "Create User",
-      href: "/create",
-      icon: Pencil,
-      variant: "ghost",
-    },
-    {
-      title: "Deposit",
-      href: "/deposit",
-      icon: PlusCircle,
-      variant: "ghost",
-    },
-    {
-      title: "Withdraw",
-      href: "/withdraw",
-      icon: MinusCircle,
-      variant: "ghost",
-    },
-  ];
+const links: NavLink[] = [
+  {
+    title: "Dashboard",
+    href: "/",
+    icon: LayoutDashboard,
+    variant: "default",
+  },
+  {
+    title: "Users",
+    href: "/users",
+    icon: UsersRound,
+    variant: "ghost",
+  },
+  {
+    title: "Loans",
+    href: "/loans",
+    icon: Landmark,
+    variant: "ghost",
+  },
+  {
+    title: "Create User",
+    href: "/create",
+    icon: Pencil,
+    variant: "ghost",
+  },
+  {
+    title: "Deposit",
+    href: "/deposit",
+    icon: PlusCircle,
+    variant: "ghost",
+  },
+  {
+    title: "Withdraw",
+    href: "/withdraw",
+    icon: MinusCircle,
+    variant: "ghost",
+  },
+];
 
   const SidebarHeader = () => (
-    <div className="mb-4 flex items-center gap-2 grayscale"></div>
+    <div className="flex items-center grayscale gap-2 mb-4">
+     
+    </div>
   );
 
   return (
-    <div
+    <div 
       className={`relative border-r pb-10 pt-6 transition-all duration-300 ${
-        isCollapsed ? "min-w-[80px] px-3" : "min-w-[200px] px-6"
+        isCollapsed ? 'min-w-[80px] px-3' : 'min-w-[200px] px-6'
       }`}
     >
       <SidebarHeader />
